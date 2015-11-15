96bea9e5a31119ca8a66a352e2b7f60612dcbf8a
marczenn <marcusareulius@gmail.com>
2015-11-14 23:02:02 -0700

    got events to work

--- gator.js
+++ gator.js
@@ -149,26 +149,25 @@ if (Meteor.isClient) {
 	    }
 
 
-	    var api = {}
+	   
 
-	    api.init = 	function init() {
-	    	api.attachClickEvents();
-	    };
 
-	    api.attachClickEvents = function attachClickEvents() {
-	    	document.getElementById('menu-icon').onclick = api.showNav
-	    	document.getElementById('nav-home-link').on('click', api.showMap)
-	    	document.getElementById('nav-posts-link').on('click', api.showPosts)
-	    }
 
-	    api.showNav = function showNav(e) {
-	    	console.log('event triggered')
-	    	document.getElementById('nav').style.display = "none"
-	    }
-
-	    api.init();
 	    
 	})();
+			function showNav() {
+	    		console.log('event triggered')
+	    		document.getElementById('nav').style.display = "block"
+	    	}
+
+	    	function init() {
+	    		document.getElementById('menu-icon').onclick = showNav
+	    		document.getElementById('nav-home-link').on('click', api.showMap)
+	    		document.getElementById('nav-posts-link').on('click', api.showPosts)
+	    	}
+
+	    	init()
+
     }
 
 
